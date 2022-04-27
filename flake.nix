27a99789c0392f011587fdc8edefcac74f14f9b3
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-28 02:05:04 +0900

    Fix defaultTemplate location.

--- flake.nix
+++ flake.nix
@@ -3,12 +3,13 @@
   outputs = { self }: {
     templates = {
 
+      default = self.templates.cabal-ghc8107;
+
       cabal-ghc8107 = {
         path = ./cabal-ghc8107/dist;
         description = "Create a haskell-nix Hello World project using the latest cabal and ghc8107";
       };
-    };
 
-    defaultTemplate = self.templates.cabal-ghc8107;
+    };
   };
 }
