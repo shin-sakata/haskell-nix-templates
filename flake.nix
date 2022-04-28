f33181d01768e37469f7cf5d7c99054bdf501130
shin-sakata <shintaro.sakata.tokyo@gmail.com>
2022-04-28 17:22:49 +0900

    fix bug.

--- flake.nix
+++ flake.nix
@@ -5,16 +5,14 @@
 
       default = self.templates.cabal-ghc8107;
 
-      cabal = {
-        ghc8107 = {
-          path = ./cabal/ghc8107/dist;
-          description = "Create a haskell-nix Hello World project using the latest cabal and ghc8107";
-        };
+      cabal-ghc8107 = {
+        path = ./cabal/ghc8107/dist;
+        description = "Create a haskell-nix Hello World project using the latest cabal and ghc8107";
+      };
 
-        ghc902 = {
-          path = ./cabal/ghc902/dist;
-          description = "Create a haskell-nix Hello World project using the latest cabal and ghc902";
-        };
+      cabal-ghc902 = {
+        path = ./cabal/ghc902/dist;
+        description = "Create a haskell-nix Hello World project using the latest cabal and ghc902";
       };
 
     };
